52cf36ff315c0da4c5b2bfa215fc8fdea2372dfa
Kim Tore Jensen <kimtjen@gmail.com>
2026-01-20 15:00:59 +0100

    Mockup JITA entitlement listing

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,40 @@ enum Commands {
     Jita,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Jita => jita::run().unwrap(),
+        Commands::Jita => match jita::run().await {
+            Ok(_) => {}
+            Err(err) => println!("JITA error: {err}"),
+        },
     }
 }
 
 pub mod jita {
-    #[derive(Clone, Debug)]
-    pub enum Error {}
+    use google_cloud_gax::paginator::ItemPaginator;
+    use google_cloud_privilegedaccessmanager_v1::client::PrivilegedAccessManager;
 
-    pub fn run() -> Result<(), Error> {
+    #[derive(Debug, thiserror::Error)]
+    pub enum Error {
+        #[error("Init: {0}")]
+        Builder(#[from] google_cloud_gax::client_builder::Error),
+        #[error("Request: {0}")]
+        Request(#[from] google_cloud_privilegedaccessmanager_v1::Error),
+    }
+
+    pub async fn run() -> Result<(), Error> {
+        println!("List entitlements");
+        let client = PrivilegedAccessManager::builder().build().await?;
+        let mut item_iterator = client
+            .list_entitlements()
+            .set_parent("folders/739335424622/locations/global")
+            .by_item();
+        while let Some(item) = item_iterator.next().await {
+            let item = item?;
+            println!("Entitlement: {}", item.name);
+        }
         Ok(())
     }
 }
