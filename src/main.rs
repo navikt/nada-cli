54f6fc3062fd9d4271e009d5d5e6b875324c64c1
Kim Tore Jensen <kimtjen@gmail.com>
2026-01-20 13:59:28 +0100

    Command-line library boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+/// Nada command-line interface.
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum Commands {
+    /// Gain just-in-time access for Google Cloud privileged resources.
+    Jita,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Jita => jita::run().unwrap(),
+    }
+}
+
+pub mod jita {
+    #[derive(Clone, Debug)]
+    pub enum Error {}
+
+    pub fn run() -> Result<(), Error> {
+        Ok(())
+    }
 }
